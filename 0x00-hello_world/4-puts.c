5b0e1e20adf3d10a03b33d17e9f109b1f7d652aa
Adejumoke Nna <nnaadejumoke@gmail.com>
2022-03-10 15:14:45 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,6 @@
-#include <stdio.h>
-
-/**
-
-* main - print to std output "with proper grammar, but the outcome
-
-*	 is a piece of art"
-
-*
-
-* Use printf
-
-* Return: Return 0 (Without warning)
-
-*/
-
-
-
-
-
-int main(void)
-
+#Include <stdio.h>
+int main (void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
