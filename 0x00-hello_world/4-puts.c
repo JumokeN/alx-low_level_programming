64336b748382c8e38a9815bfa05c433bbee1d52a
Adejumoke Nna <nnaadejumoke@gmail.com>
2022-03-10 15:27:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,31 @@
-#Include <stdio.h>
-int main (void)
+#include <stdio.h>
+
+
+
+/**
+
+*  main - print to the standard output 'Programming is multilingual puzzle'
+
+*
+
+*  Use Puts
+
+*  Return: retrn 0
+
+*/
+
+
+
+
+
+
+
+int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
