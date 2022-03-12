fced5ea3b0b17a661f0bcda258c76a84f2da2166
Adejumoke Nna <nnaadejumoke@gmail.com>
2022-03-11 16:23:49 -0800

    putchar twice

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ *
+  * main - prints the lowercase alphabet in reverse, followed by a new line
+  */
+
+int main(void)
+
+{
+	int ch;
+	for (ch = 122; ch > 96; ch--)
+
+	{
+			putchar(ch);
+
+		}
+	putchar('\n');
+
+	return (0);
+
+}
+
+ 
