643e73dacaecd299d75de0d802fab5c95561b875
Adejumoke Nna <nnaadejumoke@gmail.com>
2022-03-11 17:07:17 -0800

    fixed indent

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,11 @@
 
 
 /**
- *
- * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line.
- */
+* main - prints all single digit numbers of base 10 starting from 0,
+* followed by a new line.
+*/
 
 int main(void)
-
 {
 	int n;
 
