2625773290d977fdba620feb9070f6f63441b3e0
Adejumoke Nna <nnaadejumoke@gmail.com>
2022-03-11 17:53:53 -0800

    fix 9

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,24 +2,16 @@
 
 
 /**
- *
-  * main - prints the lowercase alphabet in reverse, followed by a new line
-  */
+* main - prints the lowercase alphabet in reverse, followed by a new line
+*/
 
 int main(void)
-
 {
-	int ch;
-	for (ch = 122; ch > 96; ch--)
-
-	{
-			putchar(ch);
-
-		}
-	putchar('\n');
-
-	return (0);
-
+int ch;
+for (ch = 122; ch > 96; ch--)
+{
+	putchar(ch);
 }
-
- 
+	putchar('\n');
+return (0);
+} 
